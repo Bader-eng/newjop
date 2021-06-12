02077e8ad62267506680466687c3cda3bcce370d
Bader-eng <baderali2016eng@gmail.com>
2021-06-13 01:02:01 +0300

    done

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const express= require('express');
 const pg = require('pg');
 
-//const client = new pg.Client(process.env.DATABASE_URL);
-const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}} );
+const client = new pg.Client(process.env.DATABASE_URL);
+//const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}} );
 //const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}} );
 const methodOverride = require('method-override');
 
