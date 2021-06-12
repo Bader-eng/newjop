15540f308445cd8a17b9742f67b499f8768a3fd7
Bader-eng <baderali2016eng@gmail.com>
2021-06-13 00:46:23 +0300

    done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const pg = require('pg');
 
 //const client = new pg.Client(process.env.DATABASE_URL);
 const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}} );
+//const client = new pg.Client( { connectionString: process.env.DATABASE_URL, ssl: process.env.LOCALLY ? false : {rejectUnauthorized: false}} );
 const methodOverride = require('method-override');
 
 const superagent= require ('superagent');
